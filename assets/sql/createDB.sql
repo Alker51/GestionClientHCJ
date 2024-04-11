3f32b403114c147817b613979e7612b4190fe60d
Rémy Robin <remyrobin51@gmail.com>
2024-04-11 22:50:30 +0200

    🗃️  CREATE : script for create client table

---
+++ assets/sql/createDB.sql
@@ -0,0 +1,15 @@
+CREATE TABLE client (
+    id INT PRIMARY KEY,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(255),
+    adress VARCHAR(255),
+    city VARCHAR(255),
+    country VARCHAR(255),
+    username VARCHAR(255),
+    password VARCHAR(255),
+    avatar BLOB,
+    RIB VARCHAR(255),
+    BIC VARCHAR(255)
+);
